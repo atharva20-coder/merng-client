503e2a930573c9fe4c05dbeb9a99f5ea1cef9fdd
atharvajoshi <official.atharva20@gmail.com>
2020-12-14 13:19:12 +0530

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: 'https://murmuring-castle-65219.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
